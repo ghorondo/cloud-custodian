faed8dc420c598047230b9c149052d2dc6b1a22a
g4omar <g4omar@gmail.com>
2025-04-03 04:24:42 -0400

    Adding Comprehend resources to Custodian

---
+++ c7n/resources/comprehend.py
@@ -0,0 +1,93 @@
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+
+from c7n.actions import BaseAction
+from c7n.manager import resources
+from c7n.query import QueryResourceManager, TypeInfo, DescribeSource
+from c7n.utils import local_session, type_schema
+from c7n.tags import universal_augment
+from c7n.filters.vpc import VpcFilter
+
+
+@resources.register('comprehend-endpoint')
+class ComprehendEndpoint(QueryResourceManager):
+
+    class resource_type(TypeInfo):
+        service = 'comprehend'
+        enum_spec = ('list_endpoints', 'EndpointPropertiesList', None)
+        detail_spec = (
+            'describe_endpoint', 'EndpointArn', 'EndpointArn', None)
+        arn = id = 'EndpointArn'
+        name = 'EndpointArn'
+        date = 'CreationTime'
+        permission_prefix = 'comprehend'
+        universal_taggable = object()
+
+
+class ComprehendEntityRecognizerDescribe(DescribeSource):
+
+    def augment(self, resources):
+        return universal_augment(self.manager, super().augment(resources))
+
+
+@resources.register('comprehend-entity-recognizer')
+class ComprehendEntityRecognizer(QueryResourceManager):
+
+    class resource_type(TypeInfo):
+        service = 'comprehend'
+        enum_spec = ('list_entity_recognizers', 'EntityRecognizerPropertiesList', None)
+        detail_spec = (
+            'describe_entity_recognizer', 'EntityRecognizerArn', 'EntityRecognizerArn', None)
+        arn = id = 'EntityRecognizerArn'
+        name = 'EntityRecognizerArn'
+        date = 'SubmitTime'
+        permission_prefix = 'comprehend'
+        universal_taggable = object()
+
+    source_mapping = {'describe': ComprehendEntityRecognizerDescribe}
+
+
+class ComprehendDocumentClassifierDescribe(DescribeSource):
+
+    def augment(self, resources):
+        return universal_augment(self.manager, super().augment(resources))
+
+
+@resources.register('comprehend-document-classifier')
+class ComprehendDocumentClassifier(QueryResourceManager):
+
+    class resource_type(TypeInfo):
+        service = 'comprehend'
+        enum_spec = ('list_document_classifiers', 'DocumentClassifierPropertiesList', None)
+        detail_spec = (
+            'describe_document_classifier', 'DocumentClassifierArn', 'DocumentClassifierArn', None)
+        arn = id = 'DocumentClassifierArn'
+        name = 'DocumentClassifierArn'
+        date = 'SubmitTime'
+        permission_prefix = 'comprehend'
+        universal_taggable = object()
+
+    source_mapping = {'describe': ComprehendDocumentClassifierDescribe}
+
+
+class ComprehendFlywheelDescribe(DescribeSource):
+
+    def augment(self, resources):
+        return universal_augment(self.manager, super().augment(resources))
+
+
+@resources.register('comprehend-flywheel')
+class ComprehendFlywheel(QueryResourceManager):
+
+    class resource_type(TypeInfo):
+        service = 'comprehend'
+        enum_spec = ('list_flywheels', 'FlywheelSummaryList', None)
+        detail_spec = (
+            'describe_flywheel', 'FlywheelArn', 'FlywheelArn', None)
+        arn = id = 'FlywheelArn'
+        name = 'FlywheelArn'
+        date = 'CreationTime'
+        permission_prefix = 'comprehend'
+        universal_taggable = object()
+
+    source_mapping = {'describe': ComprehendFlywheelDescribe}
---
+++ tests/test_comprehend.py
@@ -0,0 +1,80 @@
+# Copyright The Cloud Custodian Authors.
+# SPDX-License-Identifier: Apache-2.0
+from .common import BaseTest
+
+
+class TestComprehendEndpoint(BaseTest):
+
+       def test_comprehend_endpoint_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_endpoint_vpc")
+        p = self.load_policy(
+            {
+                "name": "list-comprehend-endpoints", 
+                "resource": "comprehend-endpoint",
+                "filters": [
+                    {"type": "value", "key": "VpcConfig", "value": "present"}
+                ],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('EndpointArn' in resources[0])
+        self.assertTrue('VpcConfig' in resources[0])
+
+
+class TestComprehendEntityRecognizer(BaseTest):
+    def test_comprehend_entity_recognizer_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_entity_recognizer_vpc")
+        p = self.load_policy(
+            {
+                "name": "list-comprehend-recognizers", 
+                "resource": "comprehend-entity-recognizer",
+                "filters": [
+                    {"type": "value", "key": "VpcConfig", "value": "present"}
+                ],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('EntityRecognizerArn' in resources[0])
+        self.assertTrue('VpcConfig' in resources[0])
+
+
+class TestComprehendDocumentClassifier(BaseTest):
+
+    def test_comprehend_document_classifier_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_document_classifier_vpc")
+        p = self.load_policy(
+            {
+                "name": "comprehend-document-classifier-vpc",
+                "resource": "comprehend-document-classifier",
+                "filters": [
+                    {"type": "value", "key": "VpcConfig", "value": "present"}
+                ],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('VpcConfig' in resources[0])
+
+
+class TestComprehendFlywheel(BaseTest):
+       def test_comprehend_flywheel_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_flywheel_vpc")
+        p = self.load_policy(
+            {
+                "name": "list-comprehend-flywheels", 
+                "resource": "comprehend-flywheel",
+                "filters": [
+                    {"type": "value", "key": "VpcConfig", "value": "present"}
+                ],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('FlywheelArn' in resources[0])
+        self.assertTrue('VpcConfig' in resources[0])
