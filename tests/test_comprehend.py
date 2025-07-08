2c9f3d158da2d4373a91d0ac83ba3df8ce083985
g4omar <g4omar@gmail.com>
2025-07-08 13:41:37 -0400

    Adding KMS filter and job resource types

--- tests/test_comprehend.py
+++ tests/test_comprehend.py
@@ -97,6 +97,20 @@ class ComprehendEntityRecognizerTests(BaseTest):
         resources = p.run()
         self.assertEqual(len(resources), 1)
 
+    def test_comprehend_entity_recognizer_kms_key(self):
+        session_factory = self.replay_flight_data("test_comprehend_entity_recognizer_kms_key")
+        p = self.load_policy(
+            {
+                "name": "comprehend-entity-recognizer-kms",
+                "resource": "comprehend-entity-recognizer",
+                "filters": [{"type": "kms-key"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue(resources[0]['VolumeKmsKeyId'])
+
 
 class ComprehendDocumentClassifierTests(BaseTest):
 
@@ -145,6 +159,20 @@ class ComprehendDocumentClassifierTests(BaseTest):
         tags = client.list_tags_for_resource(ResourceArn=arn)
         self.assertEqual(len(tags.get("Tags", [])), 0)
 
+    def test_comprehend_document_classifier_kms_key(self):
+        session_factory = self.replay_flight_data("test_comprehend_document_classifier_kms_key")
+        p = self.load_policy(
+            {
+                "name": "comprehend-document-classifier-kms",
+                "resource": "comprehend-document-classifier",
+                "filters": [{"type": "kms-key"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue(resources[0]['VolumeKmsKeyId'])
+
 
 class ComprehendFlywheelTests(BaseTest):
     def test_comprehend_flywheel_vpc(self):
@@ -184,5 +212,327 @@ class ComprehendFlywheelTests(BaseTest):
         self.assertEqual(len(resources), 1)
         client = session_factory().client("comprehend")
         arn = resources[0]["FlywheelArn"]
+        tags = client.list_tags_for_resource(ResourceArn=arn)
+        self.assertEqual(len(tags.get("Tags", [])), 0)
+
+
+# Job Tests
+
+class ComprehendEntitiesDetectionJobTests(BaseTest):
+    def test_comprehend_entities_detection_job_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_entities_detection_job_vpc")
+        p = self.load_policy(
+            {
+                "name": "comprehend-entities-detection-job-vpc",
+                "resource": "comprehend-entities-detection-job",
+                "filters": [{"type": "value", "key": "VpcConfig", "value": "present"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('VpcConfig' in resources[0])
+
+    def test_comprehend_entities_detection_job_tag_untag(self):
+        session_factory = self.replay_flight_data("test_comprehend_entities_detection_job_tag_untag")
+        p = self.load_policy(
+            {
+                "name": "comprehend-entities-detection-job-tags",
+                "resource": "comprehend-entities-detection-job",
+                "filters": [{"tag:ASV": "PolicyTestASV"}],
+                "actions": [{"type": "remove-tag", "tags": ["ASV"]}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        client = session_factory().client("comprehend")
+        arn = resources[0]["JobArn"]
+        tags = client.list_tags_for_resource(ResourceArn=arn)
+        self.assertEqual(len(tags.get("Tags", [])), 0)
+
+
+class ComprehendSentimentDetectionJobTests(BaseTest):
+    def test_comprehend_sentiment_detection_job_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_sentiment_detection_job_vpc")
+        p = self.load_policy(
+            {
+                "name": "comprehend-sentiment-detection-job-vpc",
+                "resource": "comprehend-sentiment-detection-job",
+                "filters": [{"type": "value", "key": "VpcConfig", "value": "present"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('VpcConfig' in resources[0])
+
+    def test_comprehend_sentiment_detection_job_tag_untag(self):
+        session_factory = self.replay_flight_data("test_comprehend_sentiment_detection_job_tag_untag")
+        p = self.load_policy(
+            {
+                "name": "comprehend-sentiment-detection-job-tags",
+                "resource": "comprehend-sentiment-detection-job",
+                "filters": [{"tag:ASV": "PolicyTestASV"}],
+                "actions": [{"type": "remove-tag", "tags": ["ASV"]}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        client = session_factory().client("comprehend")
+        arn = resources[0]["JobArn"]
+        tags = client.list_tags_for_resource(ResourceArn=arn)
+        self.assertEqual(len(tags.get("Tags", [])), 0)
+
+
+class ComprehendDocumentClassificationJobTests(BaseTest):
+    def test_comprehend_document_classification_job_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_document_classification_job_vpc")
+        p = self.load_policy(
+            {
+                "name": "comprehend-document-classification-job-vpc",
+                "resource": "comprehend-document-classification-job",
+                "filters": [{"type": "value", "key": "VpcConfig", "value": "present"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('VpcConfig' in resources[0])
+
+    def test_comprehend_document_classification_job_tag_untag(self):
+        session_factory = self.replay_flight_data("test_comprehend_document_classification_job_tag_untag")
+        p = self.load_policy(
+            {
+                "name": "comprehend-document-classification-job-tags",
+                "resource": "comprehend-document-classification-job",
+                "filters": [{"tag:ASV": "PolicyTestASV"}],
+                "actions": [{"type": "remove-tag", "tags": ["ASV"]}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        client = session_factory().client("comprehend")
+        arn = resources[0]["JobArn"]
+        tags = client.list_tags_for_resource(ResourceArn=arn)
+        self.assertEqual(len(tags.get("Tags", [])), 0)
+
+
+class ComprehendTopicsDetectionJobTests(BaseTest):
+    def test_comprehend_topics_detection_job_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_topics_detection_job_vpc")
+        p = self.load_policy(
+            {
+                "name": "comprehend-topics-detection-job-vpc",
+                "resource": "comprehend-topics-detection-job",
+                "filters": [{"type": "value", "key": "VpcConfig", "value": "present"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('VpcConfig' in resources[0])
+
+    def test_comprehend_topics_detection_job_tag_untag(self):
+        session_factory = self.replay_flight_data("test_comprehend_topics_detection_job_tag_untag")
+        p = self.load_policy(
+            {
+                "name": "comprehend-topics-detection-job-tags",
+                "resource": "comprehend-topics-detection-job",
+                "filters": [{"tag:ASV": "PolicyTestASV"}],
+                "actions": [{"type": "remove-tag", "tags": ["ASV"]}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        client = session_factory().client("comprehend")
+        arn = resources[0]["JobArn"]
+        tags = client.list_tags_for_resource(ResourceArn=arn)
+        self.assertEqual(len(tags.get("Tags", [])), 0)
+
+
+class ComprehendDominantLanguageDetectionJobTests(BaseTest):
+    def test_comprehend_dominant_language_detection_job_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_dominant_language_detection_job_vpc")
+        p = self.load_policy(
+            {
+                "name": "comprehend-dominant-language-detection-job-vpc",
+                "resource": "comprehend-dominant-language-detection-job",
+                "filters": [{"type": "value", "key": "VpcConfig", "value": "present"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('VpcConfig' in resources[0])
+
+    def test_comprehend_dominant_language_detection_job_tag_untag(self):
+        session_factory = self.replay_flight_data("test_comprehend_dominant_language_detection_job_tag_untag")
+        p = self.load_policy(
+            {
+                "name": "comprehend-dominant-language-detection-job-tags",
+                "resource": "comprehend-dominant-language-detection-job",
+                "filters": [{"tag:ASV": "PolicyTestASV"}],
+                "actions": [{"type": "remove-tag", "tags": ["ASV"]}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        client = session_factory().client("comprehend")
+        arn = resources[0]["JobArn"]
+        tags = client.list_tags_for_resource(ResourceArn=arn)
+        self.assertEqual(len(tags.get("Tags", [])), 0)
+
+
+class ComprehendKeyPhrasesDetectionJobTests(BaseTest):
+    def test_comprehend_key_phrases_detection_job_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_key_phrases_detection_job_vpc")
+        p = self.load_policy(
+            {
+                "name": "comprehend-key-phrases-detection-job-vpc",
+                "resource": "comprehend-key-phrases-detection-job",
+                "filters": [{"type": "value", "key": "VpcConfig", "value": "present"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('VpcConfig' in resources[0])
+
+    def test_comprehend_key_phrases_detection_job_tag_untag(self):
+        session_factory = self.replay_flight_data("test_comprehend_key_phrases_detection_job_tag_untag")
+        p = self.load_policy(
+            {
+                "name": "comprehend-key-phrases-detection-job-tags",
+                "resource": "comprehend-key-phrases-detection-job",
+                "filters": [{"tag:ASV": "PolicyTestASV"}],
+                "actions": [{"type": "remove-tag", "tags": ["ASV"]}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        client = session_factory().client("comprehend")
+        arn = resources[0]["JobArn"]
+        tags = client.list_tags_for_resource(ResourceArn=arn)
+        self.assertEqual(len(tags.get("Tags", [])), 0)
+
+
+class ComprehendPiiEntitiesDetectionJobTests(BaseTest):
+    def test_comprehend_pii_entities_detection_job_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_pii_entities_detection_job_vpc")
+        p = self.load_policy(
+            {
+                "name": "comprehend-pii-entities-detection-job-vpc",
+                "resource": "comprehend-pii-entities-detection-job",
+                "filters": [{"type": "value", "key": "VpcConfig", "value": "present"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('VpcConfig' in resources[0])
+
+    def test_comprehend_pii_entities_detection_job_tag_untag(self):
+        session_factory = self.replay_flight_data("test_comprehend_pii_entities_detection_job_tag_untag")
+        p = self.load_policy(
+            {
+                "name": "comprehend-pii-entities-detection-job-tags",
+                "resource": "comprehend-pii-entities-detection-job",
+                "filters": [{"tag:ASV": "PolicyTestASV"}],
+                "actions": [{"type": "remove-tag", "tags": ["ASV"]}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        client = session_factory().client("comprehend")
+        arn = resources[0]["JobArn"]
+        tags = client.list_tags_for_resource(ResourceArn=arn)
+        self.assertEqual(len(tags.get("Tags", [])), 0)
+
+    def test_comprehend_pii_entities_detection_job_kms_key(self):
+        session_factory = self.replay_flight_data("test_comprehend_pii_entities_detection_job_kms_key")
+        p = self.load_policy(
+            {
+                "name": "comprehend-pii-job-kms",
+                "resource": "comprehend-pii-entities-detection-job",
+                "filters": [{"type": "kms-key"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue(resources[0]['OutputDataConfig']['KmsKeyId'])
+
+
+class ComprehendEventsDetectionJobTests(BaseTest):
+    def test_comprehend_events_detection_job_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_events_detection_job_vpc")
+        p = self.load_policy(
+            {
+                "name": "comprehend-events-detection-job-vpc",
+                "resource": "comprehend-events-detection-job",
+                "filters": [{"type": "value", "key": "VpcConfig", "value": "present"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('VpcConfig' in resources[0])
+
+    def test_comprehend_events_detection_job_tag_untag(self):
+        session_factory = self.replay_flight_data("test_comprehend_events_detection_job_tag_untag")
+        p = self.load_policy(
+            {
+                "name": "comprehend-events-detection-job-tags",
+                "resource": "comprehend-events-detection-job",
+                "filters": [{"tag:ASV": "PolicyTestASV"}],
+                "actions": [{"type": "remove-tag", "tags": ["ASV"]}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        client = session_factory().client("comprehend")
+        arn = resources[0]["JobArn"]
+        tags = client.list_tags_for_resource(ResourceArn=arn)
+        self.assertEqual(len(tags.get("Tags", [])), 0)
+
+
+class ComprehendTargetedSentimentDetectionJobTests(BaseTest):
+    def test_comprehend_targeted_sentiment_detection_job_vpc(self):
+        session_factory = self.replay_flight_data("test_comprehend_targeted_sentiment_detection_job_vpc")
+        p = self.load_policy(
+            {
+                "name": "comprehend-targeted-sentiment-detection-job-vpc",
+                "resource": "comprehend-targeted-sentiment-detection-job",
+                "filters": [{"type": "value", "key": "VpcConfig", "value": "present"}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertTrue('VpcConfig' in resources[0])
+
+    def test_comprehend_targeted_sentiment_detection_job_tag_untag(self):
+        session_factory = self.replay_flight_data("test_comprehend_targeted_sentiment_detection_job_tag_untag")
+        p = self.load_policy(
+            {
+                "name": "comprehend-targeted-sentiment-detection-job-tags",
+                "resource": "comprehend-targeted-sentiment-detection-job",
+                "filters": [{"tag:ASV": "PolicyTestASV"}],
+                "actions": [{"type": "remove-tag", "tags": ["ASV"]}],
+            },
+            session_factory=session_factory,
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        client = session_factory().client("comprehend")
+        arn = resources[0]["JobArn"]
         tags = client.list_tags_for_resource(ResourceArn=arn)
         self.assertEqual(len(tags.get("Tags", [])), 0)
