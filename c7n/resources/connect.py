b3f7a5cd4c53f7588ed671ba500ab2deffc228b9
g4omar <g4omar@gmail.com>
2025-08-14 15:27:39 -0700

    update connect.py

--- c7n/resources/connect.py
+++ c7n/resources/connect.py
@@ -1,12 +1,11 @@
 # Copyright The Cloud Custodian Authors.
 # SPDX-License-Identifier: Apache-2.0
 from c7n.manager import resources
-from c7n.query import QueryResourceManager, TypeInfo
-from c7n.filters import ValueFilter
+from c7n.query import QueryResourceManager, TypeInfo, ChildResourceManager
+from c7n.filters import ValueFilter, CrossAccountAccessFilter
 from c7n.utils import local_session, type_schema
 from c7n.actions import Action
 from c7n.filters.kms import KmsRelatedFilter
-from c7n.filters import CrossAccountAccessFilter
 from c7n.resolver import ValuesFrom
 
 
@@ -64,53 +63,54 @@ class ConnectInstanceAttributeFilter(ValueFilter):
 
         return results
 
-    @Connect.action_registry.register("set-attributes")
-    class SetAttributes(Action):
-        """Set the attributes for the connect resources
 
-        :example:
+@Connect.action_registry.register("set-attributes")
+class SetAttributes(Action):
+    """Set the attributes for the connect resources
 
-        .. code-block:: yaml
+    :example:
 
-            policies:
-              - name: connect-set-contact-lens
-                resource: connect-instance
-                filters:
-                  - type: instance-attribute
-                    key: Attribute.Value
-                    value: false
-                    attribute_type: CONTACT_LENS
-                actions:
-                  - type: set-attributes
-                    attribute_type: CONTACT_LENS
-                    value: true
-              - name: connect-disable-contact-lens
-                resource: connect-instance
-                filters:
-                  - type: instance-attribute
-                    key: Attribute.Value
-                    value: true
-                    attribute_type: CONTACT_LENS
-                actions:
-                  - type: set-attributes
-                    attribute_type: CONTACT_LENS
-                    value: false
-        """
-        attributes = ["INBOUND_CALLS", "OUTBOUND_CALLS",
-                      "CONTACTFLOW_LOGS", "CONTACT_LENS",
-                      "AUTO_RESOLVE_BEST_VOICES", "USE_CUSTOM_TTS_VOICES",
-                      "EARLY_MEDIA", "MULTI_PARTY_CONFERENCE",
-                      "HIGH_VOLUME_OUTBOUND", "ENHANCED_CONTACT_MONITORING"]
-        schema = type_schema("set-attributes", attribute_type={'anyOf': [{'enum': attributes},
-                  {'type': 'string'}]}, value={}, required=["value", "attribute_type"])
-        permissions = ("connect:UpdateInstanceAttribute",)
+    .. code-block:: yaml
 
-        def process(self, resources):
-            client = local_session(self.manager.session_factory).client('connect')
+        policies:
+          - name: connect-set-contact-lens
+            resource: connect-instance
+            filters:
+              - type: instance-attribute
+                key: Attribute.Value
+                value: false
+                attribute_type: CONTACT_LENS
+            actions:
+              - type: set-attributes
+                attribute_type: CONTACT_LENS
+                value: true
+          - name: connect-disable-contact-lens
+            resource: connect-instance
+            filters:
+              - type: instance-attribute
+                key: Attribute.Value
+                value: true
+                attribute_type: CONTACT_LENS
+            actions:
+              - type: set-attributes
+                attribute_type: CONTACT_LENS
+                value: false
+    """
+    attributes = ["INBOUND_CALLS", "OUTBOUND_CALLS",
+                  "CONTACTFLOW_LOGS", "CONTACT_LENS",
+                  "AUTO_RESOLVE_BEST_VOICES", "USE_CUSTOM_TTS_VOICES",
+                  "EARLY_MEDIA", "MULTI_PARTY_CONFERENCE",
+                  "HIGH_VOLUME_OUTBOUND", "ENHANCED_CONTACT_MONITORING"]
+    schema = type_schema("set-attributes", attribute_type={'anyOf': [{'enum': attributes},
+              {'type': 'string'}]}, value={}, required=["value", "attribute_type"])
+    permissions = ("connect:UpdateInstanceAttribute",)
 
-            for r in resources:
-                client.update_instance_attribute(InstanceId=r["Id"],
-                    AttributeType=self.data.get("attribute_type"), Value=self.data.get("value"))
+    def process(self, resources):
+        client = local_session(self.manager.session_factory).client('connect')
+
+        for r in resources:
+            client.update_instance_attribute(InstanceId=r["Id"],
+                AttributeType=self.data.get("attribute_type"), Value=self.data.get("value"))
 
 
 @resources.register('connect-campaign')
@@ -142,34 +142,21 @@ class ConnectCampaignKmsFilter(KmsRelatedFilter):
 
 
 @resources.register('connect-analytics-association')
-class ConnectAnalyticsAssociation(QueryResourceManager):
+class ConnectAnalyticsAssociation(ChildResourceManager):
     """Resource manager for Connect Analytics Data Associations.
-
     """
 
     class resource_type(TypeInfo):
         service = 'connect'
+        parent_spec = ('connect-instance', 'InstanceId', None)
+        enum_spec = ('list_analytics_data_associations', 'Results', None)
         arn = id = 'AssociationId'
         name = 'DataSetId'
-        enum_spec = ('list_instances', 'InstanceSummaryList', None)
+
     permissions = (
         'connect:ListInstances',
         'connect:ListAnalyticsDataAssociations',
     )
-
-    def resources(self, query=None):
-        """
-        Overrides the default resources() method to handle the multi-step
-        enumeration required for analytics data associations.
-        """
-        client = local_session(self.session_factory).client('connect')
-        instance_paginator = client.get_paginator('list_instances')
-        all_associations = []
-        for page in instance_paginator.paginate():
-            for instance in page.get('InstanceSummaryList', []):
-                response = client.list_analytics_data_associations(InstanceId=instance['Id'])
-                all_associations.extend(response.get('Results', []))
-        return self.filter_resources(all_associations)
 
 
 @ConnectAnalyticsAssociation.filter_registry.register('cross-account')
